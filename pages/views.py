34178d572481a39f3dce073520ca03375d0ea638
Lloyd Chapple <lloydchaps@gmail.com>
2021-06-07 17:22:56 +0100

    init

---
+++ pages/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    # return HttpResponse("<h1>Hello World!</h1>") # string of HTML code
+    return render(request, "home.html", {})
+def contact_view(request, *args, **kwargs):
+    # return HttpResponse("<h1>Contact View!</h1>") # string of HTML code
+    return render(request, "contact.html", {})
